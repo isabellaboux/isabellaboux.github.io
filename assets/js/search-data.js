aceea21a1a1afc07d20d5b956fff85f2dc7ad75e
isabellaboux <49644498+isabellaboux@users.noreply.github.com>
2026-03-21 19:39:10 +0000

    Deploying to gh-pages from @ isabellaboux/isabellaboux.github.io@4a1cffa64ded1967bbfe894f70a1cca15d164557 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-instantaneous-emotion-sharing",
+          title: 'Instantaneous Emotion Sharing',
+          description: "EEG-based detection of communicative intent in language",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
